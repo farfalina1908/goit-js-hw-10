034df24922dd4fe9490cad7a0aade35b6c5e8d8e
Elena Berezina <farfalina1908@gmail.com>
2023-03-24 16:00:13 +0200

    changed fetchCountries.js

--- src/modules/fetchCountries.js
+++ src/modules/fetchCountries.js
@@ -3,7 +3,7 @@ export function fetchCountries(name) {
    return fetch (`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
    .then(response => {
     if(!response.ok) {
-        throw new Error(response.status === 404);
+        throw new Error(response.statusText);
     }
     // console.log(response.json());
     return response.json();
